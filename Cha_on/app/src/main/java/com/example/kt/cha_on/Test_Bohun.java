6b450f4a6a9f0fde9c89257c18d951456b2d879b
ktKIM <b11kkt@nate.com>
2015-11-22 19:16:39 +0900

    edit

--- Cha_on/app/src/main/java/com/example/kt/cha_on/Test_Bohun.java
+++ Cha_on/app/src/main/java/com/example/kt/cha_on/Test_Bohun.java
@@ -7,13 +7,8 @@ public class Test_Bohun {
 
     int test1 =1;
 
-<<<<<<< HEAD
-    int kyeongtae =3;
-    int a = 1;
 
-=======
-    int bohun =1;
-    
->>>>>>> 6c2665fda9b6a7235d90675b91360e1b76667f7e
+    int bohun =2;
+
 
 }
